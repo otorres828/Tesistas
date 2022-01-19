8269c52f1e15ef8aa50c7beff66ce2870f806982
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-01-18 18:34:07 -0900

    Actualizar AdminLTE

--- public/Views/escuela/index.php
+++ public/Views/escuela/index.php
@@ -26,7 +26,7 @@
   <!-- summernote -->
   <link rel="stylesheet" href="../../plugins/summernote/summernote-bs4.min.css">
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="sidebar-mini layout-fixed vsc-initialized layout-navbar-fixed sidebar-closed sidebar-collapse">
 <div class="wrapper">
 
   <!-- Preloader -->
