cf682ce82f48b7fa894a0ba122e6f0de03969b57
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-01-18 16:09:40 -0900

    errores

--- Core/Error.php
+++ Core/Error.php
@@ -44,11 +44,7 @@ class Error
         http_response_code($code);
 
         if (\App\Config::SHOW_ERRORS) {
-            echo "<h1>Fatal errores</h1>";
-            echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
-            echo "<p>Message: '" . $exception->getMessage() . "'</p>";
-            echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
-            echo "<p>Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
+            View::render('errores/404.php');
         } else {
             $log = dirname(__DIR__) . '/logs/' . date('Y-m-d') . '.txt';
             ini_set('error_log', $log);
---
+++ public/Views/errores/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Error</title>
+</head>
+<body>
+<section class="vh-100 d-flex align-items-center justify-content-center">
+    <div class="container">
+        <div class="row align-items-center ">
+            <div class="col-12 col-lg-5 order-2 order-lg-1 text-center text-lg-left">
+                <h1 class="mt-5">Algo ha salido mal <span class="text-primary">Pagina</span> no encontrada</h1>
+                <p class="lead my-4">Haga click en Back para regresar.</p>
+                <a href="/login" class="btn btn-gray-800 d-inline-flex align-items-center justify-content-center mb-4">
+                    <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
+                    Back Regresar
+                </a>
+            </div>
+            <div class="col-12 col-lg-7 order-1 order-lg-2 text-center d-flex align-items-center justify-content-center">
+                <img class="img-fluid w-75" src="../../Views/errores/404.svg" alt="404 Server Error">
+            </div>
+        </div>
+    </div>
+</section>   
+</body>
+</html>
