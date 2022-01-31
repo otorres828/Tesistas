674441ade0919daf71766acd2f4cad7b9126fb1b
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-01-31 18:43:44 -0400

    modificar login

--- public/Views/autenticacion/login.php
+++ public/Views/autenticacion/login.php
@@ -11,14 +11,14 @@
     <!-- Bootstrap CSS -->
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="../../dist/css/login.css">
 
 </head>
 
 <body class="text-center">
     <div class="container ">
 
-        <div class="pt-10">
+        <div class="pt-10 form-signin">
             <form  action="login-iniciar" method="POST">
 
                 <?php session_start(); if (isset($_SESSION['mensaje'])) { ?>
@@ -50,8 +50,6 @@
             </form>
         </div>
     </div>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
