5836e3740d1d54d52c7000a279c29d146e0f62ec
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-02-01 11:48:04 -0400

    tesistas/perfil validacion hecha de numTel y Correo

--- App/Controllers/tesistas/TesistasController.php
+++ App/Controllers/tesistas/TesistasController.php
@@ -7,85 +7,113 @@ use App\Models\PropuestaTG;
 use App\Models\Tesistas;
 use \Core\View;
 
-class TesistasController extends \Core\Controller{
+class TesistasController extends \Core\Controller
+{
 
-    public function index() {
+    public function index()
+    {
         $this->autenticar();
         $this->modificarCodigo();
-        $tesista=(new Auth())->where('cedula','=',$_SESSION['cedula'])->getOb();
-        $mispropuestas=(new PropuestaTG())->mispropuestas($_SESSION['cedula']);
-        View::render('tesistas/index.php',['tesista'=>$tesista,
-                                            'mispropuestas'=>$mispropuestas
-                                            ]); 
+        $tesista = (new Auth())->where('cedula', '=', $_SESSION['cedula'])->getOb();
+        $mispropuestas = (new PropuestaTG())->mispropuestas($_SESSION['cedula']);
+        View::render('tesistas/index.php', [
+            'tesista' => $tesista,
+            'mispropuestas' => $mispropuestas
+        ]);
     }
 
-    public function perfil(){
+    public function perfil()
+    {
         $this->autenticar();
-        $tesista=(new Tesistas())->query();
-        View::render('tesistas/perfil.php',['tesista'=>$tesista]); 
+        $tesista = (new Tesistas())->query();
+        View::render('tesistas/perfil.php', ['tesista' => $tesista]);
     }
 
-    public function propuestasaprobadas(){
-        $autenticacion= new Auth();
+    public function propuestasaprobadas()
+    {
+        $autenticacion = new Auth();
         $autenticacion->verificado();
-        $autenticacion->rol('Tesistas'); 
+        $autenticacion->rol('Tesistas');
 
-        $tesista=(new Auth())->where('cedula','=',$_SESSION['cedula'])->getOb();
-        $propuestasaprobadas="";
-        View::render('tesistas/propuestasaprobadas.php',['tesista'=>$tesista,
-                                                         'propuestasaprobadas'=>$propuestasaprobadas]);
+        $tesista = (new Auth())->where('cedula', '=', $_SESSION['cedula'])->getOb();
+        $propuestasaprobadas = "";
+        View::render('tesistas/propuestasaprobadas.php', [
+            'tesista' => $tesista,
+            'propuestasaprobadas' => $propuestasaprobadas
+        ]);
     }
 
-    public function modificarClave(){
+    public function modificarClave()
+    {
         $this->autenticar();
-        if(isset($_POST['modificarclave'])){
-            if(isset($_POST['claveactual']) && isset($_POST['nuevaclave'])){
+        if (isset($_POST['modificarclave'])) {
+            if (isset($_POST['claveactual']) && isset($_POST['nuevaclave'])) {
                 echo "existen las claves;";
             }
         }
     }
-    public function modificarCorreo(){
-        
-        if(isset($_POST['modificarcorreo'])){
-            if(isset($_POST['correo'])){
-                (new Tesistas())->modificarCorreo($_POST['correo']);
-                $_SESSION['mensaje']="Se modifico el correo con exito";
-                $_SESSION['colorcito']="success";
-                header('location:tesista-perfil');
-            }
-        }
-    }
+    public function modificarCorreo()
+    {
 
-    public function modificarTelefono(){
-        
-        if(isset($_POST['modificartelefono'])){
-            if(isset($_POST['telefono'])){
-                (new Tesistas())->modificarTelefono($_POST['telefono']);
-                $_SESSION['mensaje']="Se modifico el telefono con exito";
-                $_SESSION['colorcito']="success";
-                header('location:tesista-perfil');
-            }
-        }
-    }
+        if (isset($_POST['modificarcorreo'])) {
+            if (isset($_POST['correo'])) {
+                $resultado = (new Auth())->where('correo', '=', $_POST['correo'])->getOb();
 
-    public function modificarCodigo(){
-        if(isset($_POST['modificarcodigo'])){
-                $key = "";
-                $pattern = "1234567890abcdefghijklmnopqrstuvwxyz";
-                $max = strlen($pattern)-1;
-                for($i = 0; $i < 26; $i++){
-                    $key .= substr($pattern, mt_rand(0,$max), 1);
+                if ($resultado > 0) {
+                    session_start();
+                    $_SESSION['mensaje'] = "Correo ya registrado, intente con otro correo";
+                    $_SESSION['colorcito'] = "danger";
+                } else {
+                    (new Tesistas())->modificarCorreo($_POST['correo']);
+                    $_SESSION['mensaje'] = "Se modifico el correo con exito";
+                    $_SESSION['colorcito'] = "success";
                 }
-                (new Tesistas())->modificarcodigo($key);
-                $_SESSION['mensaje']="Se modifico el codigo con exito";
-                $_SESSION['colorcito']="info";
+
                 header('location:tesista-perfil');
+            }
         }
     }
-  
-    
-    private function autenticar(){
-        $autenticacion= new Auth();
+
+    public function modificarTelefono()
+    {
+
+        if (isset($_POST['modificartelefono'])) {
+            if (isset($_POST['telefono'])) {
+                $resultado = (new Tesistas())->where('telefono', '=', $_POST['telefono'])->getOb();
+                if ($resultado > 0) {
+                    session_start();
+                    $_SESSION['mensaje'] = "Telefono ya registrado, intente con otro numero";
+                    $_SESSION['colorcito'] = "danger";
+                } else {
+                    (new Tesistas())->modificarTelefono($_POST['telefono']);
+                    $_SESSION['mensaje'] = "Se modifico el telefono con exito";
+                    $_SESSION['colorcito'] = "success";
+                }
+                header('location:tesista-perfil');
+            }
+        }
+    }
+
+    public function modificarCodigo()
+    {
+        if (isset($_POST['modificarcodigo'])) {
+            $key = "";
+            $pattern = "1234567890abcdefghijklmnopqrstuvwxyz";
+            $max = strlen($pattern) - 1;
+            for ($i = 0; $i < 26; $i++) {
+                $key .= substr($pattern, mt_rand(0, $max), 1);
+            }
+            (new Tesistas())->modificarcodigo($key);
+            $_SESSION['mensaje'] = "Se modifico el codigo con exito";
+            $_SESSION['colorcito'] = "info";
+            header('location:tesista-perfil');
+        }
+    }
+
+
+    private function autenticar()
+    {
+        $autenticacion = new Auth();
         $autenticacion->verificado();
         $autenticacion->rol('Tesistas');
     }
--- public/Views/tesistas/propuestasaprobadas.php
+++ public/Views/tesistas/propuestasaprobadas.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Tesista | Propuestas Aprobadas</title>
-  <?php include_once('../public/Views/componentes/cssadminlte.php');?>
+  <?php include_once('../public/Views/componentes/cssadminlte.php'); ?>
   <!-- DATATABLES -->
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
 </head>
@@ -12,172 +13,171 @@
 <body class="sidebar-mini layout-fixed vsc-initialized layout-navbar-fixed sidebar-closed sidebar-collapse">
 
   <div class="wrapper">
-      <!-- Preloader -->
-      <!-- <div class="preloader flex-column justify-content-center align-items-center">
+    <!-- Preloader -->
+    <!-- <div class="preloader flex-column justify-content-center align-items-center">
       <img class="animation__shake" src="../../dist/img/Ucabg.png" alt="Ucab Guayana" height="30%" width="15%">
       </div> -->
 
-      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-          </li>
-        </ul>
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
 
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-
-          <!-- Notifications Dropdown Menu -->
-          <li class="nav-item dropdown">
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="#">
             <i class="fas fa-cog"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <span class="dropdown-item dropdown-header"><?php echo $tesista['nombre_usuario'];?></span>
+            <span class="dropdown-item dropdown-header"><?php echo $tesista['nombre_usuario']; ?></span>
             <div class="dropdown-divider"></div>
             <a href="tesista-perfil" class="dropdown-item">
               <i class="fas fa-user"></i> Perfil
             </a>
             <div class="dropdown-divider"></div>
             <a href="login-cerrarsesion" class="dropdown-item">
-            <i class="fas fa-sign-out-alt"></i>Cerrar Sesion
+              <i class="fas fa-sign-out-alt"></i>Cerrar Sesion
             </a>
-            
+
           </div>
         </li>
-          <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-              <i class="fas fa-expand-arrows-alt"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-              <i class="fas fa-th-large"></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-          <img src="../../dist/img/Ucabg.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">UCABG</span>
-        </a>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+            <i class="fas fa-th-large"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
 
-        <!-- Sidebar -->
-        <div class="sidebar">
-          <!-- Sidebar user panel (optional) -->
-          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-              <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-              <a href="#" class="d-block">Tesista</a>
-            </div>
-          </div>   
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link">
+        <img src="../../dist/img/Ucabg.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">UCABG</span>
+      </a>
 
-          <!-- Sidebar Menu -->
-          <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <a href="tesista-perfil" class="d-block"><?php echo $tesista['nombre_usuario']; ?></a>
+          </div>
+        </div>
 
-              <li class="nav-item">
-                <a href="tesistas" class="nav-link">
-                  <i class="nav-icon fas fa-th"></i>
-                  <p>
-                    Panel de Control
-                  </p>
-                </a>
-              </li>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+            <li class="nav-item">
+              <a href="tesistas" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                  Panel de Control
+                </p>
+              </a>
+            </li>
 
 
-              <li class="nav-item">
-                <a href="tesista-propuestas-aprobadas" class="nav-link">
-                  <i class="nav-icon fas fa-table"></i>
-                  <p>
-                    Propuestas Aprobadas
-                  </p>
-                </a>
-            
-              </li>
-       
-              <li class="nav-item">
-                <a href="#" class="nav-link">
+            <li class="nav-item">
+              <a href="tesista-propuestas-aprobadas" class="nav-link">
+                <i class="nav-icon fas fa-table"></i>
+                <p>
+                  Propuestas Aprobadas
+                </p>
+              </a>
+
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
                 <i class="fas fa-balance-scale"></i>
-                  <p>
-                    Perfil
-                    <i class="fas fa-angle-left right"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="pages/search/simple.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Todos</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="pages/search/enhanced.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Cargar Comites</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
+                <p>
+                  Perfil
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="pages/search/simple.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Todos</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/search/enhanced.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Cargar Comites</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
 
+          </ul>
+        </nav>
+      </div>
+      <!-- /.sidebar -->
+    </aside>
 
-            </ul>
-          </nav>
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Panel de Control</h1>
+            </div><!-- /.col -->
+
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="float-sm"><strong>Mis Propuestas Aprobadas</strong></h1>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
         </div>
-        <!-- /.sidebar -->
-      </aside>
 
-      <div class="content-wrapper">
-        <div class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1 class="m-0">Panel de Control</h1>
-              </div><!-- /.col -->
+        <div class="p-4 card table-responsive py-4">
+          <table class="table table-flush" id="example">
+            <thead class="thead-light">
+              <tr class="text-center">
+                <th>Nº </th>
+                <th>Titulo</th>
+                <th>Modalidad</th>
+                <th>Observaciones</th>
+                <th>Tutor Academico</th>
+              </tr>
+            </thead>
+            <tbody>
 
-            </div><!-- /.row -->
-          </div><!-- /.container-fluid -->
+            </tbody>
+          </table>
         </div>
-        
-        <section class="content">  
-          <div class="container-fluid">
-              <div class="row mb-2">
-                <div class="col-sm-6">
-                  <h1 class="float-sm"><strong>Mis Propuestas Aprobadas</strong></h1>
-                </div><!-- /.col -->
-              </div><!-- /.row -->
-          </div>
+      </section>
+    </div>
 
-          <div class="p-4 card table-responsive py-4">
-            <table class="table table-flush" id="example">
-                <thead class="thead-light">
-                <tr class="text-center">
-                    <th>Nº </th>
-                    <th>Titulo</th>
-                    <th>Modalidad</th>
-                    <th>Observaciones</th>
-                    <th>Tutor Academico</th>
-                </tr>
-                </thead>
-                <tbody>
-                  
-                </tbody>
-            </table>
-          </div>
-        </section>
-      </div>   
-      
-      <?php include_once('../public/Views/componentes/footer.php');?>
+    <?php include_once('../public/Views/componentes/footer.php'); ?>
 
   </div>
 
-  <?php include_once('../public/Views/componentes/adminlte.php');?>
-  <?php include_once('../public/Views/componentes/scripdatatable.php');?>
+  <?php include_once('../public/Views/componentes/adminlte.php'); ?>
+  <?php include_once('../public/Views/componentes/scripdatatable.php'); ?>
 </body>
+
 </html>
