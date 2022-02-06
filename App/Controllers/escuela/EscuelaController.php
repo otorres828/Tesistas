55fde19b64a75fcd18605e2f1f7db477f2d1d59c
Jesus Alfonzo <jesusalfonzo97@gmail.com>
2022-02-06 00:16:35 -0400

    Escuela - ver todos los tesistas

--- App/Controllers/escuela/EscuelaController.php
+++ App/Controllers/escuela/EscuelaController.php
@@ -4,13 +4,28 @@ namespace App\Controllers\escuela;
 
 use App\Models\Auth;
 use \Core\View;
+use App\Models\Tesistas;
 
-class EscuelaController extends \Core\Controller{
 
-    public function index() {
+
+class EscuelaController extends \Core\Controller
+{
+
+    public function index()
+    {
         $this->autenticar();
         View::render('escuela/index.php');
     }
+
+
+    // ver todos los tesistas en escuela/tesistas
+    public function tesistasTodos()
+    {
+        $this->autenticar();
+        $tesistas = (new Tesistas())->get();
+        View::render('escuela/tesistas-todos.php', ['tesistas' => $tesistas]);
+    }
+
 
     private function autenticar()
     {
@@ -18,5 +33,4 @@ class EscuelaController extends \Core\Controller{
         $autenticacion->verificado();
         $autenticacion->rol('Escuela');
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,24 @@ $router->add('login-iniciar', ['controller' => 'autenticacion\AuthController', '
 $router->add('login-cerrarsesion', ['controller' => 'autenticacion\AuthController', 'method' => 'cerrarSesion']);
 
 //TESISTAS
-$router->add('tesistas',['controller' => 'tesistas\TesistasController','method'=>'index']);
-$router->add('tesista-perfil',['controller' => 'tesistas\TesistasController','method'=>'perfil']);
-$router->add('tesista-perfil-modificarTelefono',['controller' => 'tesistas\TesistasController','method'=>'modificarTelefono']);
-$router->add('tesista-perfil-modificarCodigo',['controller' => 'tesistas\TesistasController','method'=>'modificarCodigo']);
-$router->add('tesista-perfil-modificarCorreo',['controller' => 'tesistas\TesistasController','method'=>'modificarCorreo']);
-$router->add('tesista-perfil-modificarClave',['controller' => 'tesistas\TesistasController','method'=>'modificarClave']);
-$router->add('tesistas-guardar-propuesta',['controller' => 'tesistas\TesistasController','method'=>'guardarpropuesta']);
+$router->add('tesistas', ['controller' => 'tesistas\TesistasController', 'method' => 'index']);
+$router->add('tesista-perfil', ['controller' => 'tesistas\TesistasController', 'method' => 'perfil']);
+$router->add('tesista-perfil-modificarTelefono', ['controller' => 'tesistas\TesistasController', 'method' => 'modificarTelefono']);
+$router->add('tesista-perfil-modificarCodigo', ['controller' => 'tesistas\TesistasController', 'method' => 'modificarCodigo']);
+$router->add('tesista-perfil-modificarCorreo', ['controller' => 'tesistas\TesistasController', 'method' => 'modificarCorreo']);
+$router->add('tesista-perfil-modificarClave', ['controller' => 'tesistas\TesistasController', 'method' => 'modificarClave']);
+$router->add('tesistas-guardar-propuesta', ['controller' => 'tesistas\TesistasController', 'method' => 'guardarpropuesta']);
 
-$router->add('tesista-propuestas-aprobadas',['controller' => 'tesistas\TesistasController','method'=>'propuestasaprobadas']);
+$router->add('tesista-propuestas-aprobadas', ['controller' => 'tesistas\TesistasController', 'method' => 'propuestasaprobadas']);
 
 //PROFESORES
-$router->add('profesores',['controller'=>'profesores\ProfesorController','method'=>'index']);
-$router->add('profesor-perfil',['controller'=>'profesores\ProfesorController','method'=>'perfil']);
+$router->add('profesores', ['controller' => 'profesores\ProfesorController', 'method' => 'index']);
+$router->add('profesor-perfil', ['controller' => 'profesores\ProfesorController', 'method' => 'perfil']);
 
 
 //ESCUELA
 $router->add('escuela', ['controller' => 'escuela\EscuelaController', 'method' => 'index']);
+$router->add('escuela/tesistas', ['controller' => 'escuela\EscuelaController', 'method' => 'tesistasTodos']);
 
 
 $router->dispatch($_SERVER['QUERY_STRING']);
