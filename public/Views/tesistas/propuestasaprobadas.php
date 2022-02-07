cf2261258e339560c2a421dc2c3e654ae1fa0057
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-02-06 23:14:31 -0400

    modificar datos del compañero

--- public/Views/tesistas/propuestasaprobadas.php
+++ public/Views/tesistas/propuestasaprobadas.php
@@ -7,9 +7,9 @@
   <title>Tesista | Propuestas Aprobadas</title>
   <?php
 
-use App\Models\PropuestaTG;
+  use App\Models\PropuestaTG;
 
-include_once('../public/Views/componentes/cssadminlte.php'); ?>
+  include_once('../public/Views/componentes/cssadminlte.php'); ?>
   <!-- DATATABLES -->
 </head>
 
@@ -106,20 +106,20 @@ include_once('../public/Views/componentes/cssadminlte.php'); ?>
       </div>
       <!-- /.sidebar -->
     </aside>
-      <div class="content-wrapper">
-        <div class="content-header">
-          <div class="container-fluid">
-          <?php foreach($propuestasaprobadas as $propuestas){?>
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <?php foreach ($propuestasaprobadas as $propuestas) { ?>
 
             <div class="row">
               <div class="col-12 col-xl-8">
                 <div class="card card-body border-0 shadow mb-4">
-                  <h2 class="h5 mb-4">Nro Correlativo: <strong><?php echo $propuestas['num_c'];?></strong></h2>
+                  <h2 class="h5 mb-4">Nro Correlativo: <strong><?php echo $propuestas['num_c']; ?></strong></h2>
                   <div class="row">
                     <div class="col-md-12 mb-3">
                       <div>
                         <label for="first_name">Titulo de la Propuesta</label>
-                        <input class="form-control" type="text" value=" <?php echo $propuestas['titulo'];?>" disabled>
+                        <input class="form-control" type="text" value=" <?php echo $propuestas['titulo']; ?>" disabled>
                       </div>
                     </div>
 
@@ -128,25 +128,14 @@ include_once('../public/Views/componentes/cssadminlte.php'); ?>
                     <div class="col-md-6 mb-3">
                       <div class="form-group">
                         <label>Modalidad</label>
-                        <?php if ($propuestas['modalidad']=='I'){?>
+                        <?php if ($propuestas['modalidad'] == 'I') { ?>
                           <input class="form-control" value="INSTRUMENTAL" disabled>
-                          <?php }else{?>
-                            <input class="form-control" value="EXPERIMENTAL" disabled>
-                            <?php }?>
+                        <?php } else { ?>
+                          <input class="form-control" value="EXPERIMENTAL" disabled>
+                        <?php } ?>
                       </div>
                     </div>
-                    <div class="col-md-6 mb-3">
-                      <div class="form-group">
-                        <label for="email">Tutor Academico</label>
-                        <input class="form-control" type="email" value="" disabled>
-                      </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                      <div class="form-group">
-                        <label>Fecha de Aprobacion</label>
-                        <input class="form-control" value="" disabled>
-                      </div>
-                    </div>
+
                     <div class="col-md-6 mb-3">
                       <div class="form-group ">
                         <label>Fecha de Presentacion</label>
@@ -154,18 +143,11 @@ include_once('../public/Views/componentes/cssadminlte.php'); ?>
 
                       </div>
                     </div>
-                    <div class="col-md-6">
-                      <label>Estatus</label>
-                      <input class="form-control w-20" disabled></input>
-                    </div>
-                    <div class="col-md-6">
-                      <label>Nota Final</label>
-                      <input class="form-control w-20" disabled></input>
-                    </div>
+                
                   </div>
-                   <?php
-                   $num_c=$propuestas['num_c'];
-                   $sql="SELECT COUNT(p.num_c) as cuenta
+                  <?php
+                  $num_c = $propuestas['num_c'];
+                  $sql = "SELECT COUNT(p.num_c) as cuenta
                           FROM presentan as p, evaluacioncomite as ec,evaluacionconsejo as ecj
                           where p.num_c=$num_c
                             and p.num_c=ecj.num_c
@@ -173,11 +155,11 @@ include_once('../public/Views/componentes/cssadminlte.php'); ?>
                             and ec.estatus='APROBADO'
                             and ecj.estatus='APROBADO'
                    ";
-                   $valor=(new PropuestaTG())->sentenciaObj($sql);
-                   $valor=$valor['cuenta'];
-                   if($valor==2){
-                       $cedula=$_SESSION['cedula'];
-                       $sql="SELECT (p.cedula)
+                  $valor = (new PropuestaTG())->sentenciaObj($sql);
+                  $valor = $valor['cuenta'];
+                  if ($valor == 2) {
+                    $cedula = $_SESSION['cedula'];
+                    $sql = "SELECT (p.cedula)
                               FROM presentan as p, evaluacioncomite as ec,evaluacionconsejo as ecj
                               where p.num_c=$num_c
                                 and p.cedula!=$cedula
@@ -185,41 +167,61 @@ include_once('../public/Views/componentes/cssadminlte.php'); ?>
                                 and p.num_C=ec.num_c
                                 and ec.estatus='APROBADO'
                                 and ecj.estatus='APROBADO'";
-                       $valor=(new PropuestaTG())->sentenciaObj($sql);
-                       $valor=$valor['cedula'];
-                       $sql="SELECT cedula,nombre,correoucab,telefono FROM tesistas WHERE cedula=".$valor."";
-                       $valor=(new PropuestaTG())->sentenciaObj($sql);
-                       ?>
-                        <h2 class="h5 mb-4 mt-4">Datos del Compañero</h2>
-                        <div class="row align-items-center">
-                    <div class="col-md-6 mb-3">
-                      <div class="form-group">
-                        <label>Cedula</label>
-                       <input class="form-control" value="<?php echo $valor['cedula'];?>" disabled>
+                    $valor = (new PropuestaTG())->sentenciaObj($sql);
+                    $valor = $valor['cedula'];
+                    $sql = "SELECT cedula,nombre,correoucab,telefono FROM tesistas WHERE cedula=" . $valor . "";
+                    $valor = (new PropuestaTG())->sentenciaObj($sql);
+                  ?>
+                    <h2 class="h5 mb-4">Datos del Compañero</h2>
+                    <div class="row align-items-center">
+                      <div class="col-md-6 mb-3">
+                        <div class="form-group">
+                          <label>Cedula</label>
+                          <input class="form-control" value="<?php echo $valor['cedula']; ?>" disabled>
+                        </div>
                       </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                      <div class="form-group">
-                        <label for="email">Nombre</label>
-                        <input class="form-control" value="<?php echo $valor['nombre'];?>" disabled>
+                      <div class="col-md-6 mb-3">
+                        <div class="form-group">
+                          <label for="email">Nombre</label>
+                          <input class="form-control" value="<?php echo $valor['nombre']; ?>" disabled>
+                        </div>
                       </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                      <div class="form-group">
-                        <label>Correo Ucab</label>
-                       <input class="form-control" value="<?php echo $valor['correoucab'];?>" disabled>
+                      <div class="col-md-6 mb-3">
+                        <div class="form-group">
+                          <label>Correo Ucab</label>
+                          <input class="form-control" value="<?php echo $valor['correoucab']; ?>" disabled>
+                        </div>
                       </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                      <div class="form-group">
-                        <label for="email">Telefono</label>
-                        <input class="form-control" value="<?php echo $valor['telefono'];?>" disabled>
+                      <div class="col-md-6 mb-3">
+                        <div class="form-group">
+                          <label for="email">Telefono</label>
+                          <input class="form-control" value="<?php echo $valor['telefono']; ?>" disabled>
+                        </div>
                       </div>
+
+                      <div class="col-md-6 mb-3">
+                        <div class="form-group">
+                          <label for="email">Tutor Academico</label>
+                          <input class="form-control" type="email" value="" disabled>
+                        </div>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                        <div class="form-group">
+                          <label>Fecha de Aprobacion</label>
+                          <input class="form-control" value="" disabled>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                      <label>Estatus</label>
+                      <input class="form-control w-20" disabled></input>
                     </div>
-                    
-                  </div>
-                   <?php }?>         
-                 
+                    <div class="col-md-6">
+                      <label>Nota Final</label>
+                      <input class="form-control w-20" disabled></input>
+                    </div>
+                    </div>
+                  <?php } ?>
+
 
                 </div>
               </div>
@@ -252,13 +254,13 @@ include_once('../public/Views/componentes/cssadminlte.php'); ?>
               <?php include_once('../public/Views/componentes/modificarPerfilTesista.php'); ?>
 
             </div>
-            <?php }?>
+          <?php } ?>
 
-          </div>
         </div>
-
-
       </div>
+
+
+    </div>
     <?php include_once('../public/Views/componentes/footer.php'); ?>
 
   </div>
