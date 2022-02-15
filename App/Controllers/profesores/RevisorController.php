5fd56be25c0d6e53145604e8f12cb12af95d6dd2
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-02-15 19:33:15 -0400

    protegida ruta de formulario de revisor

--- App/Controllers/profesores/RevisorController.php
+++ App/Controllers/profesores/RevisorController.php
@@ -18,11 +18,11 @@ class RevisorController extends \Core\Controller
         $this->autenticar();
         $profesor = (new Profesores())->where('cedula', '=', $_SESSION['cedula'])->getOb();
         $propuestas = (new Profesores())->propuestasRevisor($_SESSION['cedula']);
-        $roles = (new RolesUsuarios())->where('cedula','=',$_SESSION['cedula'])->get();
+        $roles = (new RolesUsuarios())->where('cedula', '=', $_SESSION['cedula'])->get();
         View::render('profesores\revisor\index.php', [
             'profesor' => $profesor,
             'propuestas' => $propuestas,
-            'roles'=>$roles
+            'roles' => $roles
         ]);
     }
 
@@ -41,14 +41,14 @@ class RevisorController extends \Core\Controller
             } else {
                 $criterios = (new Criterios())->criteriosRevIns();
             }
-            $roles = (new RolesUsuarios())->where('cedula','=',$_SESSION['cedula'])->get();
+            $roles = (new RolesUsuarios())->where('cedula', '=', $_SESSION['cedula'])->get();
             View::render('profesores\revisor\evaluar.php', [
                 'tesitas' => $tesitas,
                 'criterios' => $criterios,
                 'profesor' => $profesor,
                 'propuesta' => $propuesta,
                 'cantidad' => $cantidad,
-                'roles'=>$roles
+                'roles' => $roles
             ]);
         } else {
             header('location:error');
@@ -57,24 +57,28 @@ class RevisorController extends \Core\Controller
 
     public function formularioRevisor()
     {
-        $criterios = (new Criterios())->criteriosRevIns();
-        $num_c = $_POST['num_c'];
-        $modalidad= (new PropuestaTG())->where('num_c','=',$num_c)->getOb();
-        session_start();
-        foreach ($criterios as $criterio) {
-            $i = $criterio['id_criterio'];
-            $nota = $_POST[$i];
-            if ($modalidad['modalidad']=='I') {
-                (new Criterios())->insertarObj("INSERT INTO revisa_instrumental 
+        if (isset($_POST['num_c'])) {
+            $criterios = (new Criterios())->criteriosRevIns();
+            $num_c = $_POST['num_c'];
+            $modalidad = (new PropuestaTG())->where('num_c', '=', $num_c)->getOb();
+            session_start();
+            foreach ($criterios as $criterio) {
+                $i = $criterio['id_criterio'];
+                $nota = $_POST[$i];
+                if ($modalidad['modalidad'] == 'I') {
+                    (new Criterios())->insertarObj("INSERT INTO revisa_instrumental 
                                                  VALUES($num_c,'$i','$nota')");
-            } else {
-                (new Criterios())->insertarObj("INSERT INTO revisa_experimental 
+                } else {
+                    (new Criterios())->insertarObj("INSERT INTO revisa_experimental 
                                                 VALUES($num_c,'$i','$nota')");
+                }
             }
+            $_SESSION['mensaje'] = "Se evaluo correctamente";
+            $_SESSION['colorcito'] = "success";
+            header('location:profesor-revisor');
+        } else {
+            header('location:error');
         }
-        $_SESSION['mensaje'] = "Se evaluo correctamente";
-        $_SESSION['colorcito'] = "success";
-        header('location:profesor-revisor');
     }
 
     private function autenticar()
