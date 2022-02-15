970bdf6c25c982aafb16254dd21cdec3eb065fdb
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-02-15 17:52:07 -0400

    esta bien revisores

--- App/Controllers/profesores/RevisorController.php
+++ App/Controllers/profesores/RevisorController.php
@@ -30,37 +30,42 @@ class RevisorController extends \Core\Controller
             $num_c = $_POST['evaluar'];
             $tesitas = (new Tesistas())->tesistasdeunapropuesta($num_c);
             $profesor = (new Profesores())->where('cedula', '=', $_SESSION['cedula'])->getOb();
-            $propuesta=(new PropuestaTG())->where('num_c','=',$num_c)->getOb();
-            if($_POST['modalidad']=='E'){
-                $criterios =(new Criterios())->criteriosRevExp();
-            }else{
+            $propuesta = (new PropuestaTG())->where('num_c', '=', $num_c)->getOb();
+            if ($_POST['modalidad'] == 'E') {
+                $criterios = (new Criterios())->criteriosRevExp();
+            } else {
                 $cantidad = (new Criterios())->cantidad_criterios_rev_in();
-                $cantidad=$cantidad['cantidad'];
-                $criterios =(new Criterios())->criteriosRevIns();
+                $cantidad = $cantidad['cantidad'];
+                $criterios = (new Criterios())->criteriosRevIns();
             }
             View::render('profesores\revisor\evaluar.php', [
                 'tesitas' => $tesitas,
                 'criterios' => $criterios,
-                'profesor'=>$profesor,
-                'propuesta'=>$propuesta,
-                'cantidad'=>$cantidad
+                'profesor' => $profesor,
+                'propuesta' => $propuesta,
+                'cantidad' => $cantidad
             ]);
-
         } else {
             header('location:error');
         }
     }
 
-    public function formularioRevisor(){
-        $criterios =(new Criterios())->criteriosRevIns();
-        $num_c=$_POST['num_c'];
+    public function formularioRevisor()
+    {
+        $criterios = (new Criterios())->criteriosRevIns();
+        $num_c = $_POST['num_c'];
+        $modalidad= (new PropuestaTG())->where('num_c','=',$num_c)->get();
         session_start();
-        foreach ($criterios as $criterio) { 
-            $i=$criterio['id_criterio'];
-            $nota= $_POST[$i];
-
-            (new Criterios())->insertarObj("INSERT INTO revisa_instrumental 
-             VALUES($num_c,'$i','$nota')");
+        foreach ($criterios as $criterio) {
+            $i = $criterio['id_criterio'];
+            $nota = $_POST[$i];
+            if ($modalidad['modalidad'] = 'I') {
+                (new Criterios())->insertarObj("INSERT INTO revisa_instrumental 
+                                                 VALUES($num_c,'$i','$nota')");
+            } else {
+                (new Criterios())->insertarObj("INSERT INTO revisa_experimental 
+                                                VALUES($num_c,'$i','$nota')");
+            }
         }
         $_SESSION['mensaje'] = "Se evaluo correctamente";
         $_SESSION['colorcito'] = "success";
