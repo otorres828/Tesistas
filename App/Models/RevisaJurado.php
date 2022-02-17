b0e87b184fd3c4db42db3db6c349ddb980eaa3b3
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-02-17 03:37:43 -0400

    faltaba

--- App/Models/RevisaJurado.php
+++ App/Models/RevisaJurado.php
@@ -13,14 +13,14 @@ class RevisaJurado extends ModeloGenerico
     }
 
     //VALIDAR EXISTENCIA DE PTG PARA EL TUTOR
-    public function validadExistenciaPTG_Ins($num_c, $cedula)
+    public function validadExistenciaPTG_Ins($num_c, $cedula_jurado)
     {
         $cantidad = $this->sentenciaObj("SELECT COUNT (ptg.num_c) AS cantidad 
-                                        FROM es_jurado_instrumental as eji,es_evaluado_por_jurado AS eej,propuestatg AS ptg
-                                        WHERE ptg.num_c=$num_c
-                                        AND ptg.num_c=eji.num_c
-                                        AND eej.cedula=$cedula
-                                        AND eji.cedula=eej.cedula_jurado");
+        FROM es_jurado_instumental as eje,es_evaluado_por_jurado AS eej,propuestatg AS ptg
+        WHERE ptg.num_c=$num_c
+        AND ptg.num_c=eje.num_c
+        AND eje.cedula=$cedula_jurado	
+        AND eje.cedula = eej.cedula_jurado");
         $cantidad = $cantidad['cantidad'];
         return $cantidad;
     }
